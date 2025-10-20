681e862c01cb57abd3cf90c02024be981988074a
alfrednazhiyampara <an634@cornell.edu>
2025-10-20 03:19:37 +0000

    my first commit

---
+++ CommitTest_Alfred.R
@@ -0,0 +1,17 @@
+library(gert)
+
+library(credentials)
+
+credentials::set_github_pat()
+
+# this will prompt a popup that asks you to enter your GitHub Personal Access Token.
+
+gert::git_pull() # pull most recent changes from GitHub
+
+gert::git_add(dir(all.files = TRUE)) # select any and all new files created or edited to be 'staged'
+
+# 'staged' files are to be saved anew on GitHub 
+
+gert::git_commit_all("my first commit") # save your record of file edits - called a commit
+
+gert::git_push() # push your commit to GitHub
